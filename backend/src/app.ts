c9877f8eb0f288b3732c4922f8c1f4e4b1386358
tap0k-dev <alex.semenov.russia.2008@gmail.com>
2026-06-15 01:34:11 +0500

    Дополнительная документация к App и GeoController

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,21 @@ import express, { type Application } from 'express';
 import Geolite2Service from '#services/Geolite2Service.js';
 import { geoRoutes } from '#routes/GeoRouter.js';
 
+/**
+ * Главное Express приложение.
+ * * @example
+ * const application = new App();
+ * // Инициализация (поднятие различных бд и т.д.)
+ * await application.init();
+ * // Запуск сервера
+ * application.app.listen("3000");
+ */
 export class App {
   private _app: Application;
 
   constructor() {
     this._app = express();
+    // Необходимо для работы с X-Forwaded-For
     this._app.set('trust proxy', true);
   }
 
--- backend/src/controllers/GeoController.ts
+++ backend/src/controllers/GeoController.ts
@@ -1,7 +1,23 @@
 import Geolite2Service from '#services/Geolite2Service.js';
 import express from 'express';
 
+/**
+ * Контроллер, отвечающий за запрос данных о местоположении.
+ * Получает IP-адрес с запроса, выполняет поиск в базе данных GeoLite2 City.
+ * * @example
+ * // Использование в коде
+ * const geoController = new GeoController();
+ * const geoRoutes = express.Router();
+ * geoRoutes.get('/location', geoController.getLocationByIp);
+ */
+
 export default class GeoController {
+  /**
+   *
+   * @param req
+   * @param res
+   * @returns Возвращает данные о местоположении в формате JSON или ошибку, при невозможности получить IP-адрес.
+   */
   getLocationByIp = (req: express.Request, res: express.Response) => {
     const geoService = Geolite2Service.getInstance();
     if (req.ip === undefined) {
